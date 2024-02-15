38fa72d45000763565d6f1926d773cbc1b9d4ce1
Francisco Jimenez <105317378+ImPan014@users.noreply.github.com>
2024-02-15 14:39:02 -0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,8 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
 
+    public App(){}
+    
     public static void main(String[] args) {
     if (args.length > 0) {
         String input = args[0];
